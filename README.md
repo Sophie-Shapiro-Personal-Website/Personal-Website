0453a33d934570e54654908bc5a082574ccdc761
sophshap <sophieashapiro@gmail.com>
2023-07-09 15:15:26 -0400

    Redo git push

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal-Website
+
+This is my personal website project. Users must click the "okay" button on the Welcome alert to see home page.
---
+++ javascript.js
@@ -0,0 +1,11 @@
+// Assign variable "greeting"
+// Create dialogue box with alert
+
+
+
+const greeting= "Welcome Sophie Nation!"
+
+console.log(greeting)
+
+// Dialogue Box
+alert (greeting)
